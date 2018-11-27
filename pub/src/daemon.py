242950cef6a2e0298d89f31b303581f5ddc29ac7
Ian Page Hands <iphands@gmail.com>
2018-11-27 18:45:01 -0500

    adding public ctrl server

---
+++ pub/src/daemon.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import jsonify
+from awake import wol
+
+app = Flask(__name__)
+
+@app.route("/wake_cosmo")
+def hello():
+    mac = 'D8:CB:8A:39:E2:BF'
+    wol.send_magic_packet(mac)
+    return jsonify({
+        "action": "woke cosmo"
+    })
+
+app.run()
+# app.run(ssl_context=('cert.pem', 'key.pem'))
