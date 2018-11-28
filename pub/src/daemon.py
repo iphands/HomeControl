70264a4e5ee5040e30680cfad0ae3af7ca5a8ece
Ian Page Hands <iphands@gmail.com>
2018-11-28 16:49:00 -0500

    require a token or reject

--- pub/src/daemon.py
+++ pub/src/daemon.py
@@ -1,16 +1,32 @@
+from functools import wraps
+from flask import request, make_response
 from flask import Flask
 from flask import jsonify
 from awake import wol
+import os
 import socket
 
 app = Flask(__name__)
+VALID_TOKEN = os.environ['HOME_TOKEN']
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not 'token' in request.args:
+            return make_response(jsonify({ 'msg': 'Unauthenticated, no token provided', 'status': '401' }), 401)
+        if 'deadbeef12' != request.args['token']:
+            return make_response(jsonify({ 'msg': 'Unauthorized, invalid token', 'status': '403' }), 403)
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/wake_cosmo')
+@token_required
 def wake_cosmo():
     wol.send_magic_packet('D8:CB:8A:39:E2:BF')
     return jsonify({ "action": "woke cosmo" })
 
 @app.route('/cosmo_status')
+@token_required
 def cosmo_status():
     s = socket.socket()
     s.settimeout(0.100)
